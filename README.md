60a01715e02b67775ed4c346f39cf72faf29ca5e
qiudongchao <qiudongchao@yolo24.com>
2017-03-07 17:06:40 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# bsdiff and bspatch
+
+## compile
+
+~~~bash
+~ make build
+~ cd build
+~ cmake ..
+~ make
+~~~
---
+++ bslib.h
@@ -0,0 +1,12 @@
+//
+// Created by qiudongchao on 2017/3/7.
+//
+
+#ifndef BSDIFF_BSLIB_H
+#define BSDIFF_BSLIB_H
+
+int diff(int argc, char *argv[]);
+
+int patch(int argc, char *argv[]);
+
+#endif //BSDIFF_BSLIB_H
